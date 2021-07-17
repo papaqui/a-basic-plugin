83aebff8bc85dbd2da3ae807795fd690b7445015
Fernando Papaqui <hola@rivaslevi.mx>
2021-07-17 09:11:25 -0500

    Update functionality

--- a-basic-plugin.php
+++ a-basic-plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: A basic plugin
-  Description: A basic plugin that is part of a tutorial on how to build plugins in WP
+  Description: This is part of a tutorial on how to build plugins in WP
   Version: 1.0
   Author: Fernando Papaqui
   Author URI: https://www.fernandopapaqui.dev
@@ -12,7 +12,7 @@ add_filter('the_content', 'addToEndOfPost');
 
 function addToEndOfPost($content) {
   if ( is_single() && is_main_query() ) {
-    return $content . '<p>My name is Fernando</p>';
+    return $content . '<p>Author: ' . the_autor(); . '</p>';
   } 
 
   return $content;
